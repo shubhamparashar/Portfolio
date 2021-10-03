562ba6dade2dc3983798b14f7c0f246e779ab180
Shubham Parashar <shubhamparashar210@gmail.com>
2021-10-03 19:24:56 +0530

    Hero section completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello There!!!
+        <br />
+        I'm Shubham Parashar
+      </SectionTitle>
+      <SectionText>
+        Technology and learning are the two things that drive me. I look for
+        different chances to explore and expand my horizons.
+      </SectionText>
+      <Button onClick={() => {}}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
